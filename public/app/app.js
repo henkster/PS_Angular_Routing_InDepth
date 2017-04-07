a3540033db9008b975b78c22719932851c3cae73
Steve Henke <SHenke@paylocity.com>
2017-04-06 23:00:03 -0500

    Configuring routes with ngRoute.

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,42 @@
 (function() {
 
-    var app = angular.module('app', []);
+  var app = angular.module('app', ['ngRoute']);
 
-    app.config(['$logProvider', function ($logProvider) {
+  app.config(['$logProvider', '$routeProvider', function ($logProvider, $routeProvider) {
 
-        $logProvider.debugEnabled(true);
+    $logProvider.debugEnabled(true);
 
-    }]);
+    $routeProvider
+      .when('/', {
+        controller: 'HomeController',
+        controllerAs: 'home',
+        templateUrl: '/app/templates/home.html'
+      })
+      .when('/schools', { // by default the routes are case-sensitive
+        controller: 'AllSchoolsController',
+        controllerAs: 'schools',
+        templateUrl: '/app/templates/allSchools.html'
+        //caseInsensitiveMatch: true // can use this to make case-insensitive
+      })
+      .when('/classrooms', {
+        controller: 'AllClassroomsController',
+        controllerAs: 'classrooms',
+        templateUrl: '/app/templates/allClassrooms.html'
+        // redirectTo: '/schools' // can use a simple URL to redirect
+        // redirectTo: function(params, currPath, currSearch) { // can also use a function
+        //   console.log(params); // { id: "1" } in /classrooms/1?foo=bar, if :id specified in route
+        //   console.log(currPath); // /classrooms/1
+        //   console.log(currSearch); // { foo: "bar" }
+        //   return '/';
+        // }
+      })
+      .when('/activities', {
+        controller: 'AllActivitiesController',
+        controllerAs: 'activities',
+        templateUrl: '/app/templates/allActivities.html'
+      })
+      .otherwise('/');
+
+  }]);
 
 }());
